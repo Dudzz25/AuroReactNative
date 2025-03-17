58d76729e745cf04defc3edae28dd83826372669
dudzbusran123 <dudzbusran23@gmail.com>
2025-03-17 16:46:56 +0800

    Almost Finish in the Home page. Need to debug Trending Section videos not showing

---
+++ components/Trending.jsx
@@ -0,0 +1,106 @@
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  ImageBackground,
+  Image,
+} from "react-native";
+import React, { useState } from "react";
+import * as Animatable from "react-native-animatable";
+import { ResizeMode, Video } from "expo-av";
+import { VideoView } from "expo-video";
+import { icons } from "../constants";
+
+const zoomIn = {
+  0: {
+    scale: 0.9,
+  },
+  1: {
+    scale: 1.1,
+  },
+};
+
+const zoomOut = {
+  0: {
+    scale: 1,
+  },
+  1: {
+    scale: 0.9,
+  },
+};
+
+const TrendingItems = ({ activeItem, item }) => {
+  // console.log(item.video);
+  const [play, setPlay] = useState(false);
+  // console.log(item.video);
+  return (
+    <Animatable.View
+      className="mr-5"
+      animation={activeItem === item.$id ? zoomIn : zoomOut}
+      duration={500}
+    >
+      {play ? (
+        <VideoView
+          player={{ uri: item.video }} // Vimeo embed URL
+          className="w-52 h-72 rounded-[33px] mt-3 bg-white/10"
+          useNativeControls // Show native video controls
+          shouldPlay={play} // Auto-play the video
+          onPlaybackStatusUpdate={(status) => {
+            if (status.didJustFinish) {
+              setPlay(false); // Stop playback once the video finishes
+            }
+          }}
+        />
+      ) : (
+        <TouchableOpacity
+          className="relative flex justify-center items-center"
+          activeOpacity={0.7}
+          onPress={() => setPlay(true)}
+        >
+          <ImageBackground
+            source={{
+              uri: item.thumbnail,
+            }}
+            className="w-52 h-72 rounded-[33px] my-5 overflow-hidden shadow-lg shadow-black/40"
+            resizeMode="cover"
+          />
+
+          <Image
+            source={icons.play}
+            className="w-12 h-12 absolute"
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      )}
+    </Animatable.View>
+  );
+};
+
+const Trending = ({ posts }) => {
+  const [activeItem, setActiveItem] = useState(posts[0]);
+
+  const viewableItemsChanged = ({ viewableItems }) => {
+    if (viewableItems.length > 0) {
+      setActiveItem(viewableItems[0].key);
+    }
+  };
+
+  return (
+    <FlatList
+      data={posts}
+      horizontal
+      keyExtractor={(item) => item.$id}
+      renderItem={({ item }) => (
+        <TrendingItems activeItem={activeItem} item={item} />
+      )}
+      onViewableItemsChanged={viewableItemsChanged}
+      viewabilityConfig={{
+        itemVisiblePercentThreshold: 70,
+      }}
+      contentOffset={{ x: 170 }}
+    />
+  );
+};
+
+export default Trending;
